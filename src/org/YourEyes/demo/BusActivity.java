40deaf7a0c4a0a535311e7ff7a8b44f61a6eddc3
tnwjd4623 <lsj9556@gmail.com>
2019-11-04 14:48:19 +0900

    Update GPS

--- src/org/YourEyes/demo/BusActivity.java
+++ src/org/YourEyes/demo/BusActivity.java
@@ -49,8 +49,8 @@ import java.util.regex.Pattern;
 
 public class BusActivity extends Activity {
     private Spinner sp_api;
-    private RadioGroup rg_object_type;
-    private RadioButton rb_json, rb_map;
+   // private RadioGroup rg_object_type;
+   // private RadioButton rb_json, rb_map;
     private Button bt_api_call;
     private TextView tv_data;
 
@@ -70,22 +70,20 @@ public class BusActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.bus_activity);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.bus_titlebar);
 
         init();
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
+        Init_GPS();
     }
 
     private void init() {
         context = this;
         sp_api = (Spinner) findViewById(R.id.sp_api);
-        rg_object_type = (RadioGroup) findViewById(R.id.rg_object_type);
         bt_api_call = (Button) findViewById(R.id.bt_api_call);
-        rb_json = (RadioButton) findViewById(R.id.rb_json);
-        rb_map = (RadioButton) findViewById(R.id.rb_map);
         tv_data = (TextView) findViewById(R.id.tv_data);
         sp_api.setSelection(0);
 
@@ -95,19 +93,9 @@ public class BusActivity extends Activity {
 
         bt_api_call.setOnClickListener(onClickListener);
         sp_api.setOnItemSelectedListener(onItemSelectedListener);
-        rg_object_type.setOnCheckedChangeListener(onCheckedChangeListener);
+
     }
 
-    private RadioGroup.OnCheckedChangeListener onCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-            if (rg_object_type.getCheckedRadioButtonId() == rb_json.getId()) {
-                tv_data.setText(jsonObject.toString());
-            } else if (rg_object_type.getCheckedRadioButtonId() == rb_map.getId()) {
-                tv_data.setText(mapObject.toString());
-            }
-        }
-    };
     private AdapterView.OnItemSelectedListener onItemSelectedListener = new AdapterView.OnItemSelectedListener() {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view,
@@ -125,11 +113,8 @@ public class BusActivity extends Activity {
         public void onSuccess(ODsayData oDsayData, API api) {
             jsonObject = oDsayData.getJson();
             mapObject = oDsayData.getMap();
-            if (rg_object_type.getCheckedRadioButtonId() == rb_json.getId()) {
-                tv_data.setText(jsonObject.toString());
-            } else if (rg_object_type.getCheckedRadioButtonId() == rb_map.getId()) {
-                tv_data.setText(mapObject.toString());
-            }
+            tv_data.setText(jsonObject.toString());
+
         }
 
         @Override
@@ -155,13 +140,13 @@ public class BusActivity extends Activity {
                     odsayService.requestLoadLane("0:0@12018:1:-1:-1", onResultCallbackListener);
                     break;
                 case "대중교통 정류장 검색":
-                    odsayService.requestSearchStation("11", "1000", "1:2", "10", "1", "127.0363583:37.5113295", onResultCallbackListener);
+                    odsayService.requestSearchStation("11", "1000", "1", "10", "1", "127.34408682849399:36.36633737269802", onResultCallbackListener);
                     break;
                 case "반경내 대중교통 POI 검색":
-                    getLocation();
+                   // getLocation();
                     String s_lon = Double.toString(longitude);
                     String s_lat = Double.toString(latitude);
-                    odsayService.requestPointSearch(s_lon, s_lat, "250", "1:2", onResultCallbackListener);
+                    odsayService.requestPointSearch(s_lon, s_lat, "250", "1", onResultCallbackListener);
                     break;
                 case "지도 위 대중교통 POI 검색":
                     odsayService.requestBoundarySearch("127.045478316811:37.68882830829:127.055063420699:37.6370465749586", "127.045478316811:37.68882830829:127.055063420699:37.6370465749586", "1:2", onResultCallbackListener);
@@ -176,24 +161,45 @@ public class BusActivity extends Activity {
         }
     };
 
-    //현재 위치 찾으려는 시점에서 호출하면 됨
-    public void getLocation() {
+    //처음 gps
+    public void Init_GPS() {
         if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(BusActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-            //권한을 허용하지 않는 경우
+
         } else {
             //권한을 허용한 경우
             try {
-                location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                longitude = location.getLongitude();
-                latitude = location.getLatitude();
-
-                Toast.makeText(this, longitude+" "+latitude, Toast.LENGTH_SHORT).show();
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,100,1, mLocationListener);
+                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,1, mLocationListener);
             } catch(SecurityException e) {
                 e.printStackTrace();
             }
         }
-       // Toast.makeText(this, longitude+" "+latitude, Toast.LENGTH_SHORT).show();
+
     }
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            Log.d("Location", longitude+", "+latitude);
+            Toast.makeText(getApplicationContext(), longitude+" "+latitude, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
 }

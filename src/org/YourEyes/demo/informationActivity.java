ba3843b9613e637e8cc8280e6d8b64328f9eee13
sonyeji <ths3385@naver.com>
2019-11-04 15:46:37 +0900

    update appinfo

--- src/org/YourEyes/demo/informationActivity.java
+++ src/org/YourEyes/demo/informationActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class informationActivity extends Activity{
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.information);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.information_titlebar);
+        img = (ImageView) findViewById(R.id.appinfo);
     }
 }

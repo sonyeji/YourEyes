da75c950455c9187fcbbe28240d146767a4a1478
tnwjd4623 <lsj9556@gmail.com>
2019-11-20 17:17:45 +0900

    정류장 어댑터 스크롤 오류 해결

--- src/org/YourEyes/demo/StatAdapter.java
+++ src/org/YourEyes/demo/StatAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,7 +16,7 @@ public class StatAdapter extends BaseAdapter {
     Context mContext = null;
     LayoutInflater mLayoutInfalter = null;
     ArrayList<BusActivity.Station> list;
-
+    ListView mList;
     TextView name;
     TextView id;
     public StatAdapter(Context context, ArrayList<BusActivity.Station> data) {
@@ -38,16 +39,39 @@ public class StatAdapter extends BaseAdapter {
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        CustomViewHolder holder;
         if(convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
             name = (TextView)convertView.findViewById(R.id.stationName);
             id = (TextView)convertView.findViewById(R.id.stationID);
+
+            holder = new CustomViewHolder();
+            holder.id = id;
+            holder.name = name;
+
+            convertView.setTag(holder);
         }
 
-        name.setText(list.get(position).getName());
-        id.setText("("+list.get(position).getId()+")");
+        else {
+            holder = (CustomViewHolder) convertView.getTag();
+        }
+        BusActivity.Station data = list.get(pos);
 
+        if(data != null) {
+            holder.id.setText(data.getId());
+            holder.name.setText(data.getName());
+        }
+        else {
+            holder.id.setText("");
+            holder.name.setText("");
+        }
         return convertView;
+    }
+
+    public class CustomViewHolder {
+        public TextView id;
+        public TextView name;
     }
 
 }
